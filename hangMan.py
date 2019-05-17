8415b358b98f528f8158201a266d85ad14db3f67
Zane Emerick <zaneemerick@macbook.local>
2019-05-17 10:52:34 -0600

    made hangMan game actually work in terminal

--- hangMan.py
+++ hangMan.py
@@ -23,48 +23,44 @@ def setTemporaryUnderScoresArray(): #create a list of underscores the same lengt
         temporaryUnderScoresArray.append('_')
 
 #Main code area
-def runGame():
-    chosenWordString = chooseRandomFromList(words)
 
-    print("\nWelcome to Hangman!")
-    setTemporaryUnderScoresArray()
+chosenWordString = chooseRandomFromList(words)
+print("\nWelcome to Hangman!")
+setTemporaryUnderScoresArray()
 
-    while gameRunning == True:
-        print(str(temporaryUnderScoresArray)) #Print the array that shows guessed letters
-        guessedChar = input("Take a guess: \n")
+while gameRunning == True:
+    print(str(temporaryUnderScoresArray)) #Print the array that shows guessed letters
+    guessedChar = input("Take a guess: \n")
 
-        for a in range(0, len(chosenWordString)): #if the word contains the guessed character replace the underscores
-            if chosenWordString[a] == guessedChar:
-                temporaryUnderScoresArray.pop(a)
-                temporaryUnderScoresArray.insert(a, guessedChar)
-                wordContainsChar = True
+    for a in range(0, len(chosenWordString)): #if the word contains the guessed character replace the underscores
+        if chosenWordString[a] == guessedChar:
+            temporaryUnderScoresArray.pop(a)
+            temporaryUnderScoresArray.insert(a, guessedChar)
+            wordContainsChar = True
 
-        if wordContainsChar:
-            print("The word does have a(n) " + guessedChar + " in it!\n")
+    if wordContainsChar:
+        print("The word does have a(n) " + guessedChar + " in it!\n")
+    else:
+        guessesRemaining = guessesRemaining - 1
+        print("Sorry, the word does not contain " + guessedChar)
+        print("You have " + str(guessesRemaining) + " guesses left")
+
+    if guessesRemaining == 0:
+        print("You have used all of your guesses. The correct word was " + chosenWordString)
+        gameRunning = False
+        break
+
+    for a in range(0, len(temporaryUnderScoresArray)): #test if the game array matches the word
+        if chosenWordString[a] == temporaryUnderScoresArray[a]:
+            wordsMatch = True
         else:
-            guessesRemaining = guessesRemaining - 1
-            print("Sorry, the word does not contain " + guessedChar)
-            print("You have " + str(guessesRemaining) + " guesses left")
-
-        if guessesRemaining == 0:
-            print("You have used all of your guesses. The correct word was " + chosenWordString)
-            gameRunning = False
+            wordsMatch = False
             break
 
-        for a in range(0, len(temporaryUnderScoresArray)): #test if the game array matches the word
-            if chosenWordString[a] == temporaryUnderScoresArray[a]:
-                wordsMatch = True
-            else:
-                wordsMatch = False
-                break
+    if wordsMatch == True:
+        print("\nGood job! You guessed the word correctly!")
+        print(temporaryUnderScoresArray)
+        break
 
-        if wordsMatch == True:
-            print("\nGood job! You guessed the word correctly!")
-            print(temporaryUnderScoresArray)
-            break
-
-        wordContainsChar = False
-        guessedLetters.append(guessedChar) #add the guessed character to the list
-
-if __name__ == "__main__":
-    runGame()
+    wordContainsChar = False
+    guessedLetters.append(guessedChar) #add the guessed character to the list
