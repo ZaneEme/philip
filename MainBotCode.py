43d2bd21f31475ff2785950ae4fe7d681abb2969
Zane Emerick <zaneemerick@macbook.local>
2019-05-16 21:11:23 -0600

    added basic joke telling and updated readme

--- MainBotCode.py
+++ MainBotCode.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 client = commands.Bot(command_prefix = '!')
 reverseWords = False
@@ -30,6 +31,7 @@ async def quitBot(ctx): #quit the bot
     valid_users = ['Zane#9722']
     if str(ctx.author) in valid_users:
         await ctx.send("quitting the bot")
+        await client.change_presence(status = discord.Status.offline)
         quit()
     else:
         await ctx.send("You do not have permission to enter this command")
@@ -48,11 +50,29 @@ async def ping(ctx):
 @client.command()
 async def helpMe(ctx):
     embed = discord.Embed(title = "chatBot help", description = "Some useful commands")
-    embed.add_field(name = "philicia", value = "Tells her I love her <3")
     embed.add_field(name = "!toggleReverseWords", value = "Toggles whether all words will be reversed or not")
     embed.add_field(name = "!ping", value = "Returns the bot's ping to the server")
+    embed.add_field(name = "joke", value = "tells a joke")
     await ctx.send(embed = embed)
 
+@client.command(aliases = ["joke", "telljoke"])
+async def tellJoke(ctx):
+    jokeQuestion = [
+    "What do you call bears without ears?",
+    "Why don't blind people skydive?",
+    "As a scarecrow people say I'm outstanding at my job",
+    "What do cannonballs do when they're in love?"
+    "How long is a chinese man's name"]
 
+    jokeAnswer = [
+    "B",
+    "It scares the hell out of their dogs",
+    "but hay, its in my jeans",
+    "make bb's"
+    "no, really, it is."]
+
+    jokeNumber = random.randint(0, len(jokeQuestion) - 1)
+    await ctx.send(jokeQuestion[jokeNumber])
+    await ctx.send(jokeAnswer[jokeNumber])
 
 client.run('NTc4MzY4NTI3NzY2MTI2NjEz.XN15Vw.TbHe8gQ2jGVkxbyqXTC2kiUxiS4')
