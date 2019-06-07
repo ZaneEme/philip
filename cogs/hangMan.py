548cfdc7b42ec23c67545b18900953bf837cd2f0
Zane Emerick <garqwan@gmail.com>
2019-06-07 15:04:25 -0600

    added thumbnails, cleaned code, fixed guessesRemaining==0 case

--- cogs/hangMan.py
+++ cogs/hangMan.py
@@ -25,6 +25,10 @@ class hangMan(commands.Cog):
         wordContainsChar = False
         wordList = ["banana", "choclolate", "coffee", "pancake", "orange", "waffle", "omelette", "sandwich", "linguini", "alfredo", "burrito",
                     "quesadilla", "apple", "grapefruit", "avocado", "coconut", "hamburger", "peanuts", "honey", "pudding"]
+
+        game_thumbnails = ["https://i.imgur.com/44Jb4k2.png", "https://i.imgur.com/gXczhhM.png", "https://i.imgur.com/OHkzTXu.png", "https://i.imgur.com/1cgkwRQ.png",
+                        "https://i.imgur.com/ntOTFmt.png", "https://i.imgur.com/GwmPeX7.png", "https://i.imgur.com/jy490gk.png", "https://i.imgur.com/TR4gWeH.png"]
+                        
         guessedChar = 'a'
         tempUnderscoreArray = []
         guessesLeft = 7
@@ -54,9 +58,11 @@ class hangMan(commands.Cog):
         #Embed setup for game
         gameEmbedOne = discord.Embed(title = "Welcome to Hangman!", color = 0x008200)
         gameEmbedOne.set_author(name = ctx.author)
-        gameEmbedOne.add_field(name = "Word progress", value = f"{tempUnderscoreArray}", inline = False)
+        gameEmbedOne.set_thumbnail(url = "https://i.imgur.com/44Jb4k2.png")
+        gameEmbedOne.add_field(name = "Word progress", value = f"{', '.join(tempUnderscoreArray)}", inline = False)
         gameEmbedOne.add_field(name = 'Information', value = f"No letters guessed yet.\nYou have 7 guesses remaining.", inline = False)
         gameEmbedOne.add_field(name = "\u200b", value = "Enter a letter:")
+
 
         if admin_role in ctx.author.roles: #send the word list if player is an admin and cheats are on
             if self.cheatMode:
@@ -78,19 +84,23 @@ class hangMan(commands.Cog):
                     tempUnderscoreArray.pop(a)
                     tempUnderscoreArray.insert(a, guessedChar)
                     wordContainsChar = True
-                    gameEmbedOne.set_field_at(index = 0, name = "word progress", value = f"{tempUnderscoreArray}", inline = False)
+                    gameEmbedOne.set_field_at(index = 0, name = "word progress", value = f"{', '.join(tempUnderscoreArray)}", inline = False)
 
             if wordContainsChar: #test if the word has the guesses character in it or not
                 gameEmbedOne.set_field_at(index = 1, name = "Information", value = f"The word does have a(n) {guessedChar} in it!\nYou have {guessesLeft} guesses left.", inline = False)
             else:
                 guessesLeft = guessesLeft - 1
+                gameEmbedOne.set_thumbnail(url = game_thumbnails[7 - guessesLeft])
                 gameEmbedOne.set_field_at(index = 1, name = "Information", value = f"Sorry, the word does not contain {guessedChar}.\nYou have {guessesLeft} guesses left.", inline = False)
 
 
             if guessesLeft == 0: #test if all guesses have been used
-                gameEmbedOne.set_field_at(index = 1, name = "Information", value = f"{correctIncorrectString}\n{guessesRemainingString}\nYou have used all of your guesses. The correct word was {chosenWord}.", inline = False)
+                gameEmbedOne.set_field_at(index = 1, name = "Information", value = f"You have {guessesLeft} guesses left.\nThe correct word was {chosenWord}.", inline = False)
+                gameEmbedOne.remove_field(index = 2)
+                await ctx.send(embed = gameEmbedOne)
                 gameRunning = False
                 break
+
 
             for a in range(0, len(tempUnderscoreArray)): #test if the game array matches the word
                 if chosenWord[a] == tempUnderscoreArray[a]:
@@ -101,9 +111,11 @@ class hangMan(commands.Cog):
 
             if wordsMatch == True: #if the guessed word matches the chosen word after the full for loop above, end game
                 await ctx.channel.purge(limit = 2)
-                await ctx.send(f"Good job! You guessed the word correctly with {guessesLeft} guesses remaining.")
-                await ctx.send(chosenWord)
+                gameEmbedOne.set_field_at(index = 0, name = "word progress", value = f"{' '.join(tempUnderscoreArray)}", inline = False)
+                gameEmbedOne.set_field_at(index = 1, name = "Information", value = f"Good job! You guessed the word correctly with {guessesLeft} guesses remaining.", inline = False)
+                gameEmbedOne.remove_field(index = 2)
                 gameRunning = False
+                await ctx.send(embed = gameEmbedOne)
                 break
 
             wordContainsChar = False
