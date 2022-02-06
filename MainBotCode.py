6bc91a5c03bff9dbcd04dd7966255905377de782
= <=>
2022-02-06 15:15:51 -0700

    Reformat code, hide API key, document better

--- MainBotCode.py
+++ MainBotCode.py
@@ -1,109 +1,135 @@
 import discord
 from discord.ext import commands
 import random
-import os
+import json
 
-client = commands.Bot(command_prefix = '!', help_command = None)
+# set the command prefit to "!"
+client = commands.Bot(command_prefix="!", help_command=None)
 reverseWords = False
 
+# Wait for bot to set up
 @client.event
 async def on_ready():
     print("The bot is ready")
     print(client.user)
 
+
+# Runs when any message is sent
 @client.event
-async def on_message(message): #when the bot starts
+async def on_message(message):
     global reverseWords
     channel = message.channel
 
-    if message.author == client.user:  #if the bot sent the last message, do nothing
-      return
+    if message.author == client.user:  # If the bot sent the last message, do nothing
+        return
 
-    if message.content == "Philicia" or message.content == "philicia": #if the last message was philicia
+    # If the last message sent was "Philicia", print a love letter.
+    if message.content == "Philicia" or message.content == "philicia":
         if reverseWords == True:
-            await channel.send('ε> uoy evol I')
+            await channel.send("ε> uoy evol I")
         else:
-            await channel.send('I love you <3')
+            await channel.send("I love you <3")
 
     elif reverseWords == True:
-      await channel.send(message.content[::-1])
+        await channel.send(message.content[::-1])
 
-    await client.process_commands(message) #end the onMessage loop
+    await client.process_commands(message)  # End the onMessage loop
 
-@client.command(aliases = ["quit", "quitbot", "q"])
-async def quitBot(ctx): #quit the bot
-    admin_role = discord.utils.get(ctx.guild.roles, name = "Admin") #create the role of admin
+
+# Command to make the bot quit
+@client.command(aliases=["quit", "quitbot", "q"])
+async def quitBot(ctx):
+    # Get a list of admins
+    admin_role = discord.utils.get(ctx.guild.roles, name="Admin")
+
     if admin_role in ctx.author.roles:
         await ctx.send("quitting the bot")
-        await client.change_presence(status = discord.Status.offline)
+        await client.change_presence(status=discord.Status.offline)
         quit()
     else:
         await ctx.send("You do not have permission to enter this command")
 
 
-@client.command(aliases = ["reverse", "reverseWords"]) #toggle the reversal of words
+# Toggle words being reversed
+@client.command(aliases=["reverse", "reverseWords"])
 async def toggleReverseWords(ctx):
     global reverseWords
     reverseWords = not reverseWords
-    await ctx.send('reversal of words now set to ' + str(reverseWords))
+    await ctx.send("reversal of words now set to " + str(reverseWords))
 
+
+# Post the client's ping
 @client.command()
 async def ping(ctx):
     await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
 
-@client.command(Aliases = ['help', 'Help'])
-async def helpMe(ctx):
-    helpEmbed = discord.Embed(title = "Philip help", description = "Some useful commands")
-    helpEmbed.add_field(name = "!reverse", value = "Toggles word reversal")
-    helpEmbed.add_field(name = "!ping", value = "Returns the bot's ping")
-    helpEmbed.add_field(name = "!joke", value = "Tells a joke")
-    helpEmbed.add_field(name = "!hangman", value = "Plays hangman")
-    helpEmbed.add_field(name = "!clear (num)", value = "deletes a certain number of messages")
-    await ctx.send(embed = helpEmbed)
 
-@client.command(aliases = ["joke", "telljoke"])
+# Send a help menu
+@client.command(Aliases=["help", "Help"])
+async def helpMe(ctx):
+    helpEmbed = discord.Embed(title="Philip help", description="Some useful commands")
+    helpEmbed.add_field(name="!reverse", value="Toggles word reversal")
+    helpEmbed.add_field(name="!ping", value="Returns the bot's ping")
+    helpEmbed.add_field(name="!joke", value="Tells a joke")
+    helpEmbed.add_field(name="!hangman", value="Plays hangman")
+    helpEmbed.add_field(
+        name="!clear (num)", value="deletes a certain number of messages"
+    )
+    await ctx.send(embed=helpEmbed)
+
+
+# Print out a random joke from list
+@client.command(aliases=["joke", "telljoke"])
 async def tellJoke(ctx):
     jokeQuestion = [
-    "What do you call bears without ears?",
-    "Why don't blind people skydive?",
-    "As a scarecrow people say I'm outstanding at my job.",
-    "What do cannonballs do when they're in love?",
-    "How long is a chinese man's name.",
-    "What do you get when you cross an insomniac, a dislexic, and an agnostic?",
-    "Where does the general keep his armies?",
-    "What do you call an Argentinian with a rubber toe?",
-    "What do you call a sketchy Italian neighborhood?",
-    "Why can't you hear a pterodactyl go to the bathroom?",
-    "Why do you never see elephants hiding in trees?",
-    "I told my girlfriend she drew her eyebrows too high.",
-    "Where can you find a cow with no legs?"]
-    #13 jokes
+        "What do you call bears without ears?",
+        "Why don't blind people skydive?",
+        "As a scarecrow people say I'm outstanding at my job.",
+        "What do cannonballs do when they're in love?",
+        "How long is a chinese man's name.",
+        "What do you get when you cross an insomniac, a dislexic, and an agnostic?",
+        "Where does the general keep his armies?",
+        "What do you call an Argentinian with a rubber toe?",
+        "What do you call a sketchy Italian neighborhood?",
+        "Why can't you hear a pterodactyl go to the bathroom?",
+        "Why do you never see elephants hiding in trees?",
+        "I told my girlfriend she drew her eyebrows too high.",
+        "Where can you find a cow with no legs?",
+    ]
+    # 13 jokes
 
     jokeAnswer = [
-    "B",
-    "It scares the hell out of their dogs",
-    "But hay, its in my jeans",
-    "Make bb's",
-    "No, really, it is",
-    "Someone who lays awake at night wondering of there's a dog",
-    "In his sleevies",
-    "Roberto",
-    "The Spaghetto",
-    "Because the pee is silent",
-    "Because they're so good at it",
-    "She seemed surprised",
-    "Right where you left it"]
+        "B",
+        "It scares the hell out of their dogs",
+        "But hay, its in my jeans",
+        "Make bb's",
+        "No, really, it is",
+        "Someone who lays awake at night wondering of there's a dog",
+        "In his sleevies",
+        "Roberto",
+        "The Spaghetto",
+        "Because the pee is silent",
+        "Because they're so good at it",
+        "She seemed surprised",
+        "Right where you left it",
+    ]
 
     jokeNumber = random.randint(0, len(jokeQuestion) - 1)
     await ctx.send(jokeQuestion[jokeNumber])
     await ctx.send(jokeAnswer[jokeNumber])
 
+
+# Clear a specified amount of messages from chat
 @client.command()
-async def clear(ctx, amount = 0):
-    admin_role = discord.utils.get(ctx.guild.roles, name = "Admin") #create an admin role
+async def clear(ctx, amount=0):
+    # Get a list of admins
+    admin_role = discord.utils.get(ctx.guild.roles, name="Admin")
     if admin_role in ctx.author.roles:
-        await ctx.channel.purge(limit = amount + 1)
+        await ctx.channel.purge(limit=amount + 1)
 
 
+# Load Hangman and grab Discord key from JSON config file
 client.load_extension("cogs.hangMan")
-client.run('***REMOVED***')
+with open("config.json") as f:
+    data = json.load(f)
+    client.run(data["key"])
--- cogs/hangMan.py
+++ cogs/hangMan.py
@@ -2,14 +2,19 @@ import discord
 from discord.ext import commands
 import random
 
+
 class hangMan(commands.Cog):
-    def __init__(self, client): #initialize the cog
+    def __init__(self, client):  # Initialize the cog
         self.client = client
         self.cheatMode = False
 
+    #
+    # If the user has the admin role, cheatmode allows
+    # the user to see a list of all possible words
+    #
     @commands.command()
-    async def cheatmode(self, ctx): #if the user is an admin toggle listing of the possible words
-        admin_role = discord.utils.get(ctx.guild.roles, name = "Admin")
+    async def cheatmode(self, ctx):
+        admin_role = discord.utils.get(ctx.guild.roles, name="Admin")
 
         if admin_role in ctx.author.roles:
             self.cheatMode = not self.cheatMode
@@ -20,111 +25,230 @@ class hangMan(commands.Cog):
         else:
             await ctx.send("You are not allowed to use that command.")
 
+    #
+    # Main command for the game. Chooses a random word from wordlist,
+    # and plays a game of standard Hangman.
+    #
     @commands.command()
     async def hangman(self, ctx):
         wordContainsChar = False
-        wordList = ["banana", "choclolate", "coffee", "pancake", "orange", "waffle", "omelette", "sandwich", "linguini", "alfredo",
-                    "burrito", "quesadilla", "apple", "grapefruit", "avocado", "coconut", "hamburger", "peanut", "honey", "pudding",
-                    "icecream", "broccoli", "watermelon", "burrito", "cheeseburger", "sushimi", "noodles", "barbecue", "macaroni", "sirloin",
-                    "wonton", "lemonade", "zucchini", "blueberry", "raspberry", "boisenberry", "lobster", "peanutbutter", "spaghetti", "lasagna",
-                    "breadsticks", "casserole", "gingerbread", "tamale", "cupcake", "stuffing", "ravioli", "meatballs", "croissant", "milkshake"]
+        wordList = [
+            "banana",
+            "choclolate",
+            "coffee",
+            "pancake",
+            "orange",
+            "waffle",
+            "omelette",
+            "sandwich",
+            "linguini",
+            "alfredo",
+            "burrito",
+            "quesadilla",
+            "apple",
+            "grapefruit",
+            "avocado",
+            "coconut",
+            "hamburger",
+            "peanut",
+            "honey",
+            "pudding",
+            "icecream",
+            "broccoli",
+            "watermelon",
+            "burrito",
+            "cheeseburger",
+            "sushimi",
+            "noodles",
+            "barbecue",
+            "macaroni",
+            "sirloin",
+            "wonton",
+            "lemonade",
+            "zucchini",
+            "blueberry",
+            "raspberry",
+            "boisenberry",
+            "lobster",
+            "peanutbutter",
+            "spaghetti",
+            "lasagna",
+            "breadsticks",
+            "casserole",
+            "gingerbread",
+            "tamale",
+            "cupcake",
+            "stuffing",
+            "ravioli",
+            "meatballs",
+            "croissant",
+            "milkshake",
+        ]
 
-        game_thumbnails = ["https://i.imgur.com/44Jb4k2.png", "https://i.imgur.com/gXczhhM.png", "https://i.imgur.com/OHkzTXu.png", "https://i.imgur.com/1cgkwRQ.png",
-                        "https://i.imgur.com/ntOTFmt.png", "https://i.imgur.com/GwmPeX7.png", "https://i.imgur.com/jy490gk.png", "https://i.imgur.com/TR4gWeH.png"]
+        game_thumbnails = [
+            "https://i.imgur.com/44Jb4k2.png",
+            "https://i.imgur.com/gXczhhM.png",
+            "https://i.imgur.com/OHkzTXu.png",
+            "https://i.imgur.com/1cgkwRQ.png",
+            "https://i.imgur.com/ntOTFmt.png",
+            "https://i.imgur.com/GwmPeX7.png",
+            "https://i.imgur.com/jy490gk.png",
+            "https://i.imgur.com/TR4gWeH.png",
+        ]
 
-        guessedChar = 'a'
+        guessedChar = "a"
         tempUnderscoreArray = []
         guessesLeft = 7
         chosenWord = "a"
         gameRunning = True
-        input = 'a'
+        input = "a"
         guessedLetters = []
-        admin_role = discord.utils.get(ctx.guild.roles, name = "Admin")
+        admin_role = discord.utils.get(ctx.guild.roles, name="Admin")
 
+        # Check if the last message sent was in the same channel as the bot
+        # and if the last message wasn't sent by the bot
         def check(m):
-                return m.author != self.client.user and m.channel == ctx.channel #if the last message was in same channel and not sent by bot return True
+            return m.author != self.client.user and m.channel == ctx.channel
 
-        def chooseWord(): #choose a random word from the list
+        # Choose a random word from the list
+        def chooseWord():
             nonlocal chosenWord
             chosenWord = random.choice(wordList)
             return chosenWord
 
-        def setGameUnderscores(): #turn the chosen word from chooseWord() into a list of empty spaces
-            nonlocal tempUnderscoreArray; chosenWord
+        # Turn the random word from chooseWord() into a list of empty space
+        def setGameUnderscores():
+            nonlocal tempUnderscoreArray
+            chosenWord
             for _ in range(0, len(chosenWord)):
                 tempUnderscoreArray.append("--")
-
 
         chooseWord()
         setGameUnderscores()
 
-        #Embed setup for game
-        gameEmbedOne = discord.Embed(title = "Welcome to Hangman!", color = 0x008200)
-        gameEmbedOne.set_author(name = ctx.author)
-        gameEmbedOne.set_thumbnail(url = "https://i.imgur.com/44Jb4k2.png")
-        gameEmbedOne.add_field(name = "Word progress", value = f"{', '.join(tempUnderscoreArray)}", inline = False)
-        gameEmbedOne.add_field(name = 'Information', value = f"No letters guessed yet.\nYou have 7 guesses remaining.", inline = False)
-        gameEmbedOne.add_field(name = "\u200b", value = "Enter a letter:")
+        # Embed setup for game into Discord channel
+        gameEmbedOne = discord.Embed(title="Welcome to Hangman!", color=0x008200)
+        gameEmbedOne.set_author(name=ctx.author)
+        gameEmbedOne.set_thumbnail(url="https://i.imgur.com/44Jb4k2.png")
+        gameEmbedOne.add_field(
+            name="Word progress",
+            value=f"{', '.join(tempUnderscoreArray)}",
+            inline=False,
+        )
+        gameEmbedOne.add_field(
+            name="Information",
+            value=f"No letters guessed yet.\nYou have 7 guesses remaining.",
+            inline=False,
+        )
+        gameEmbedOne.add_field(name="\u200b", value="Enter a letter:")
 
-
-        if admin_role in ctx.author.roles: #send the word list if player is an admin and cheats are on
+        # Send the word list if player is an admin and cheats are on
+        if admin_role in ctx.author.roles:
             if self.cheatMode:
                 await ctx.send(wordList)
 
+        # Main game loop
         while gameRunning == True:
-            await ctx.send(embed = gameEmbedOne)
-            input = await self.client.wait_for('message', check = check) #wait for the person who started it to guess a letter
+            await ctx.send(embed=gameEmbedOne)
+            input = await self.client.wait_for(
+                "message", check=check
+            )  # Wait for the person who started it to guess a letter
 
-            if input.content == "quitgame" or input.content == "endgame": #quit if the keyword is entered
+            if (
+                input.content == "quitgame" or input.content == "endgame"
+            ):  # Quit if the keyword is entered
                 await ctx.send("Ending the game.")
                 gameRunning = False
                 break
 
             guessedChar = input.content
 
-            for a in range(0, len(chosenWord)): #if the word contains the guessed character replace the underscores
+            # Check if the word contains guessed letter,
+            # if true replace underscores with letter
+            for a in range(0, len(chosenWord)):
                 if chosenWord[a] == guessedChar:
                     tempUnderscoreArray.pop(a)
                     tempUnderscoreArray.insert(a, guessedChar)
                     wordContainsChar = True
-                    gameEmbedOne.set_field_at(index = 0, name = "word progress", value = f"{', '.join(tempUnderscoreArray)}", inline = False)
+                    gameEmbedOne.set_field_at(
+                        index=0,
+                        name="word progress",
+                        value=f"{', '.join(tempUnderscoreArray)}",
+                        inline=False,
+                    )
 
-            if wordContainsChar: #test if the word has the guesses character in it or not
-                gameEmbedOne.set_field_at(index = 1, name = "Information", value = f"The word does have a(n) {guessedChar} in it!\nYou have {guessesLeft} guesses left.", inline = False)
+            # Test if the word has the guessed character in it or not
+            if wordContainsChar:
+                gameEmbedOne.set_field_at(
+                    index=1,
+                    name="Information",
+                    value=f"The word does have a(n) {guessedChar} in it!\nYou have {guessesLeft} guesses left.",
+                    inline=False,
+                )
             else:
+                # Change game thumbnail, and subtract a guess
                 guessesLeft = guessesLeft - 1
-                gameEmbedOne.set_thumbnail(url = game_thumbnails[7 - guessesLeft])
-                gameEmbedOne.set_field_at(index = 1, name = "Information", value = f"Sorry, the word does not contain {guessedChar}.\nYou have {guessesLeft} guesses left.", inline = False)
+                gameEmbedOne.set_thumbnail(url=game_thumbnails[7 - guessesLeft])
 
+                gameEmbedOne.set_field_at(
+                    index=1,
+                    name="Information",
+                    value=f"Sorry, the word does not contain {guessedChar}.\nYou have {guessesLeft} guesses left.",
+                    inline=False,
+                )
 
-            if guessesLeft == 0: #test if all guesses have been used
-                gameEmbedOne.set_field_at(index = 1, name = "Information", value = f"You have {guessesLeft} guesses left.\nThe correct word was {chosenWord}.", inline = False)
-                gameEmbedOne.remove_field(index = 2)
-                await ctx.send(embed = gameEmbedOne)
+            # Test if all guesses have been used and end game
+            if guessesLeft == 0:
+                gameEmbedOne.set_field_at(
+                    index=1,
+                    name="Information",
+                    value=f"You have {guessesLeft} guesses left.\nThe correct word was {chosenWord}.",
+                    inline=False,
+                )
+
+                gameEmbedOne.remove_field(index=2)
+                await ctx.send(embed=gameEmbedOne)
                 gameRunning = False
                 break
 
-
-            for a in range(0, len(tempUnderscoreArray)): #test if the game array matches the word
+            # Test if the guess array matches the chosen word
+            for a in range(0, len(tempUnderscoreArray)):
                 if chosenWord[a] == tempUnderscoreArray[a]:
                     wordsMatch = True
                 else:
                     wordsMatch = False
                     break
 
-            if wordsMatch == True: #if the guessed word matches the chosen word after the full for loop above, end game
-                await ctx.channel.purge(limit = 2)
-                gameEmbedOne.set_field_at(index = 0, name = "word progress", value = f"{' '.join(tempUnderscoreArray)}", inline = False)
-                gameEmbedOne.set_field_at(index = 1, name = "Information", value = f"Good job! You guessed the word correctly with {guessesLeft} guesses remaining.", inline = False)
-                gameEmbedOne.remove_field(index = 2)
+            # if the guessed word matches the chosen word, end game
+            if wordsMatch == True:
+                await ctx.channel.purge(limit=2)
+                gameEmbedOne.set_field_at(
+                    index=0,
+                    name="word progress",
+                    value=f"{' '.join(tempUnderscoreArray)}",
+                    inline=False,
+                )
+                gameEmbedOne.set_field_at(
+                    index=1,
+                    name="Information",
+                    value=f"Good job! You guessed the word correctly with {guessesLeft} guesses remaining.",
+                    inline=False,
+                )
+                gameEmbedOne.remove_field(index=2)
+
                 gameRunning = False
-                await ctx.send(embed = gameEmbedOne)
+                await ctx.send(embed=gameEmbedOne)
                 break
 
             wordContainsChar = False
-            guessedLetters.append(guessedChar) #add the guessed character to the list
-            gameEmbedOne.set_field_at(index = 2, name = f"guessed letters: {', '.join(guessedLetters)}", value = "Enter a letter:")
-            await ctx.channel.purge(limit = 2) #delete the last embed and quess
+            guessedLetters.append(guessedChar)  # add the guessed character to the list
+            gameEmbedOne.set_field_at(
+                index=2,
+                name=f"guessed letters: {', '.join(guessedLetters)}",
+                value="Enter a letter:",
+            )
+            await ctx.channel.purge(limit=2)  # delete the last embed and guess
+
 
 def setup(client):
     client.add_cog(hangMan(client))
